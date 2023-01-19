ec10abe6f7b0133906c92fc26d2792b40089ec53
Dariusz Okoński <darek200180@gmail.com>
2023-01-19 18:32:30 +0100

    initialization

--- app/routes/programmers.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class ProgrammersRoute extends Route {
-  model() {
-    return ['Larry Page', 'Linus Torvalds', 'Bill Getes', 'Ada Lovelace'];
-  }
-}

70e3c8194409294c72ca61dabc028b84188321af
Dariusz Okoński <darek200180@gmail.com>
2023-01-26 23:13:18 +0100

    components state and actions-component state and actions-combining arguments and state

---
+++ app/components/counter.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CounterComponent extends Component {
+  @tracked
+  count = 0;
+
+  @tracked
+  multiple = 1;
+
+  get total() {
+    return this.count * this.multiple;
+  }
+
+  @action
+  double() {
+    this.multiple = this.multiple * 2;
+  }
+
+  @action
+  change(amount) {
+    this.count = this.count + amount;
+  }
+
+  @action
+  clearBoth() {
+    this.count = 0;
+    this.multiple = 1;
+  }
+}

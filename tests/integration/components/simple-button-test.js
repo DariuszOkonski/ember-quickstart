425afb79b20f03f5018a18f98a02dd3045f4e01b
Dariusz Okoński <darek200180@gmail.com>
2023-03-14 18:00:31 +0100

    testing - testing tools - ember test selectors

---
+++ tests/integration/components/simple-button-test.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-quickstart/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | simple-button-test', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('should show icon and label', async function (assert) {
+    await render(hbs`
+      <SimpleButton
+        @icon="face"
+        @label="Hello world!"
+      />
+    `);
+
+    assert.strictEqual(
+      this.element.querySelector('.material-cions').textContent().trim(),
+      'face',
+      'The user sees the correct icon.'
+    );
+  });
+});

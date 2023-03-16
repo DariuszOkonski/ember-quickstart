70ca41fedabd5283c805526bf5e240c51d708f4d
Dariusz Okoński <darek200180@gmail.com>
2023-03-16 17:19:07 +0100

    components - looping throught lists - updating lists

--- app/components/message.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import Component from '@glimmer/component';
-
-export default class Message extends Component {
-  getSubstring(str, start, end) {
-    return str.substring(start, end);
-  }
-}

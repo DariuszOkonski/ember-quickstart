a7837e05946e13a983e71fce0c4963853470f6ab
Dariusz Okoński <darek200180@gmail.com>
2023-04-12 16:31:03 +0200

    start

--- app/helpers/substring.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import Helper from '@ember/comonent/helper';
-
-export default class Substring extends Helper {
-  compute([string, start, end]) {
-    return string.substring(start, end);
-  }
-}

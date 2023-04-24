b620b626dc9ee765336a30a32056f746bf0b06b1
Dariusz Okoński <darek200180@gmail.com>
2023-04-24 18:42:23 +0200

    combining arguments and state

--- app/components/counter.js
+++ app/components/counter.js
@@ -5,6 +5,16 @@ import { action } from '@ember/object';
 
 export default class CounterComponent extends Component {
   @tracked count = 0;
+  @tracked multiple = 1;
+
+  get total() {
+    return this.count * this.multiple;
+  }
+
+  @action
+  double() {
+    this.multiple = this.multiple * 2;
+  }
 
   @action
   increment() {
@@ -14,5 +24,10 @@ export default class CounterComponent extends Component {
   @action
   decrement() {
     this.count = this.count - 1;
+  }
+
+  @action
+  change(amount) {
+    this.count = this.count + amount;
   }
 }
